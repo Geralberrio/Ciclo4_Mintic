4537051e8799a8cce3e7c7cfe44ada16c8b6a7d9
Geralberrio <Geraldine.berrio12@gmail.com>
2021-11-17 08:01:27 -0500

    bases

---
+++ Back-Sprints/src.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+let operaciones =[];
+let balance = 0;
+const registrarOperacion = (monto, tipo, categoria, fecha) =>{
+
+}
+
+const registrarMetaAhorro = (meta)=>{
+
+}
+
+const estaMiMetaCumplida=()=>{
+
+}
+
+module.exports.registrarMetaAhorro = registrarMetaAhorro;
+module.exports.registrarOperacion = registrarOperacion;
+module.exports.estaMiMetaCumplida = estaMiMetaCumplida;
