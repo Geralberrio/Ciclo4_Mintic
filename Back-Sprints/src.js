86ca86894a3b9fb4f3882702b642e211b7b33551
Geralberrio <Geraldine.berrio12@gmail.com>
2021-11-21 11:42:29 -0500

    modificaciones

--- Back-Sprints/src.js
+++ Back-Sprints/src.js
@@ -1,24 +1,39 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-let operaciones =[];
+let operaciones=[];
 let balance = 0;
-const registrarOperacion = (monto, tipo, categoria, fecha) =>{
-    operacion_ind ={monto);
-    
-}
 
-const registrarMetaAhorro = (meta)=>{
+const registrarOperacion = (monto, tipo, categoria, fecha)=> {
+    /*Añadir objetos al arreglo*/   
+    operaciones.push({"monto":monto, "tipo":tipo, "categoria":categoria, "fecha":fecha});  
+   /* el condicional*/
+    if (tipo==='ingreso'){
+        balance=balance+monto;  
+    }else
+    {
+        {balance=balance-monto;}
+   
+    /*Guardar en loscalstorage*/
+    localStorage.setItem("operaciones", operaciones);
+    localStorage.setItem("balance", balance);   
+    }
+};
 
-}
+let metaAhorro =0;
+const registrarMetaAhorro=(meta)=>{
+    localStorage.setItem("metaAhorro", meta);
+};
 
-const estaMiMetaCumplida=()=>{
+const estaMiMetaCumplida=()=> {
+    metaAhorro = parseFloat(localStorage.getItem("metaAhorro"));
+    balance = parseFloat(localStorage.getItem("balance"));
+    if (metaAhorro<balance){
+        return "cumple";
+    }
+    else 
+    {
+        return "no cumple";
+    }
+};
 
-}
-
-module.exports.registrarMetaAhorro = registrarMetaAhorro;
-module.exports.registrarOperacion = registrarOperacion;
-module.exports.estaMiMetaCumplida = estaMiMetaCumplida;
+module.exports.estaMiMetaCumplida= estaMiMetaCumplida;
+module.exports.registrarMetaAhorro= registrarMetaAhorro;
+module.exports.registrarOperacion= registrarOperacion;
