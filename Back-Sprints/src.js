d8f5c4dbe8672fdce8c6c43c4229546ec1b9085f
Geralberrio <Geraldine.berrio12@gmail.com>
2021-11-24 21:01:34 -0500

    final sprint 3

--- Back-Sprints/src.js
+++ Back-Sprints/src.js
@@ -1,15 +1,17 @@
-let operaciones=[];
 let balance = 0;
 
 const registrarOperacion = async(monto, tipo, categoria, fecha)=>{
+    let operacion ={monto,tipo,categoria,fecha};  
+    tipo==='ingreso'? balance+=monto:balance-=monto;
     let response = await fetch("https://misiontic2022upb.vercel.app/api/personal-finance/operations",{
         method: 'POST',
         headers:{
             'Accept':'application/json',
             'Content-Type':'application/json'
         },
-        body:,
-    }):
+        body: JSON.stringify(operacion),
+    });
+    return (response.json());
 };
 
 const estaMiMetaCumplida=async()=> {
@@ -17,6 +19,7 @@ const estaMiMetaCumplida=async()=> {
         "https://misiontic2022upb.vercel.app/api/personal-finance/is-my-goal-achieved/4000"
     );
     let estaMiMetaCumplida = await response.json();
+    return estaMiMetaCumplida;
 };
 
 module.exports.estaMiMetaCumplida= estaMiMetaCumplida;
